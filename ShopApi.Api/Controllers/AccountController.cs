226fe33418ed3c2f8c84002eda7100b9310fd7e0
hassa <hassa@LAPTOP-U03CN6LR>
2023-11-10 21:20:41 +0330

    Create AccountController For Auth Operations

---
+++ ShopApi.Api/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopApi.Application.Contracts.Identity;
+using ShopApi.Application.Models.Identity;
+
+namespace ShopApi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        
+    }
+}
