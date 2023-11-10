bf32b45a3e32bbe4b2f305e6814390bb351d1247
hassa <hassa@LAPTOP-U03CN6LR>
2023-11-10 16:48:37 +0330

    Create Entity In Domain Layer And Work In Layer

---
+++ ShopApi.Domain/Category.cs
@@ -0,0 +1,16 @@
+﻿using ShopApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApi_Domain
+{
+    public class Category: BaseDomainEntity
+    {
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ ShopApi.Domain/Product.cs
@@ -0,0 +1,24 @@
+﻿using ShopApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApi_Domain
+{
+    public class Product: BaseDomainEntity
+    {
+        public string Name { get; set; }
+        public int FkCategoryId { get; set; }
+
+
+
+
+        [ForeignKey(nameof(FkCategoryId))]
+        public Category Category { get; set; }
+
+
+    }
+}
